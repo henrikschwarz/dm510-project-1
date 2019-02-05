c87c5c9f0f3f4116acf4e4ed45baafb1f9d4d608
henrikschwarz <henrik.hasmark@gmail.com>
2019-02-05 18:27:34 +0100

    finnished linked_list

--- src/linked_list.c
+++ src/linked_list.c
@@ -9,22 +9,51 @@
 #include <stdio.h>
 
 linked_list *init_linked_list() {
-	return NULL;
+	linked_list *list = malloc(sizeof(linked_list)); // Allocate space
+	return list; // List pointer
 }
 
 void add_element( linked_list *list, void *element) {
-
+	while (list->next != NULL) {
+		list = list->next; //  go to last list item
+	}
+	linked_list *n_list = init_linked_list(); // init new list
+	n_list->data = element;
+	n_list->previous = list;
+	list->next = n_list;
 }
 
 int linked_list_size(linked_list *list) {
-	return 0;
+	int count; // Counter
+	while (list->previous != NULL) { // Go to start
+		list=list->previous;
+	}
+	while (list->next != NULL){ // Go to end of list
+		count++; // Count items.
+	}
+	return count;
 }
 
 void *remove_first(linked_list *list) {
-	return NULL;
+	while (list->previous != NULL){
+		list = list->previous; // Find start of list
+	}
+	list->next->previous = NULL; // Remove reference to the first item
+	return list;
 }
 
 int remove_element(linked_list *list, void *element) {
-	return -1;
+	while (list->previous != NULL){
+		list=list->previous; // Go to first list item
+	}
+	while (list->next !=  NULL){ // Go to start of list
+		if (list->data == element){ // Find data
+			list->next->previous = list->previous; //previous item->next = item->next
+			list->previous->next= list->next; // next item->previous = item->previous
+			free(list); // Free space
+			return 0; // return found integer
+		}
+		list=list->next; // Didn't match element, go to next item
+	}
+	return -1; // Didn't find element
 }
-
