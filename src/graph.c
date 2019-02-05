79d23566c15972c5c06ce41371a08c1586d25681
Henrik Schwarz <SDU\hschw17@imada-106317.adm.c.sdu.dk>
2019-02-05 15:41:31 +0100

    added to graph.c

--- src/graph.c
+++ src/graph.c
@@ -5,14 +5,29 @@
 
 graph *read_graph(char *filename) {
 
+	graph new_graph;
+
 	FILE* graph_file = fopen(filename, "r"); // Open filename in read mode
 
-	char string[60];
+	char n_vertices[1]; // Number of vertices
+	fscanf(graph_file, "%s", n_vertices); // Get num
+	new_graph.number_vertices = (int)* n_vertices - 48; // Save it in graph
 
-	if (fgets(string, 60, graph_file) != NULL){
-		printf("%s", string);
- 	}
+	vertex *vertice  = malloc(sizeof(vertex)*new_graph.number_vertices);
 
+	for (size_t i = 0; i < new_graph.number_vertices; i++) {
+			vertex vert;
+			linked_list in;
+			linked_list out;
+
+			vert.id = (int) i;
+			vert.in_neighbours = &in;
+			vert.out_neighbours = &out;
+
+			vertice[i] = vert;
+	}
+
+	printf("%d\n", vertice);
 	return NULL;
 }
 
