1b4e0f6e6920e8a525b61b79ee13dc813832405c
Henrik Schwarz <SDU\hschw17@imada-106329.adm.c.sdu.dk>
2019-02-05 11:49:40 +0100

    started on graphs.c

--- src/graph.c
+++ src/graph.c
@@ -1,12 +1,21 @@
 #include <stdlib.h>
 #include "graph.h"
+#include <stdio.h>
 
 
 graph *read_graph(char *filename) {
+
+	FILE* graph_file = fopen(filename, "r"); // Open filename in read mode
+
+	char string[60];
+
+	if (fgets(string, 60, graph_file) != NULL){
+		printf("%s", string);
+ 	}
+
 	return NULL;
 }
 
 void print_graph(graph *g) {
 
 }
-
